be1b4f298a96a49d1d508a8d2edb5ff45ae6aef6
Collin Norwood <cnorwood7641@gmail.com>
2022-05-22 20:57:21 -0500

    flower, health bar, also i broke movement a little bit somehow ill fix it oops

--- main.py
+++ main.py
@@ -15,17 +15,19 @@ pygame.key.set_repeat(100, 100)
 grass = pygame.image.load("img/grass.png")
 water = pygame.image.load("img/water.png")
 mountian = pygame.image.load("img/mountian.png")
+flower = pygame.image.load("img/flower.png")
+honeycolor = (255, 255, 0)
 
 #map
-terrains = [{"sprite": grass, "passable": True}, {"sprite": water, "passable": True}, {"sprite": mountian, "passable": False}]
+terrains = [{"sprite": grass, "effect": "passable"}, {"sprite": water, "effect": "slow"}, {"sprite": mountian, "effect": "impassable"}, {"sprite": flower, "effect": "health"}]
 themap = [[-1 for y in range(150)] for x in range (150)]
 for x in range (0,150):
     for y in range (0,150):
         themap[y][x] = terrains[map1[y][x]]
 
-#Passable -- rework to non-boolean for upgrades probably
-def is_passable(map_x, map_y):
-	return themap[int(map_y)][int(map_x)]["passable"]
+#terrain collision
+def terrain_effect(map_x, map_y):
+	return themap[int(map_y)][int(map_x)]["effect"]
 
 
 #Bee class
@@ -46,27 +48,33 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
         self.image = pygame.image.load('img/bee.png')
-        self.rect = self.image.get_rect(
-            center=(96,96))
+        self.rect = self.image.get_rect(center=(96,96))
+        self.health = 50
+
+    #Damage
+    def damage(self, dmg):
+        self.health-=dmg
+        if self.health<1:
+            self.kill()
 
     #Keys - will be redone
     def update(self, key):
         global yshift
         global xshift
-        
+            
         if key==1073741904: #Left
-            if self.rect.left <= 64:
+            if self.rect.left <= 64: #If you're at the edge scroll the map instead of moving
                 self.rect.left=64
                 xshift+=64
                 playerx=self.rect.left-xshift
                 playery=self.rect.top-yshift
-                if not is_passable(playerx/64,playery/64):
+                if terrain_effect(playerx/64,playery/64)=="impassable": #undo if you hit a wall
                     xshift-=64           
             else:
-                self.rect.move_ip(-64, 0)
+                self.rect.move_ip(-64, 0) #if you're not at the edge just move left
                 playerx=self.rect.left-xshift
                 playery=self.rect.top-yshift
-                if not is_passable(playerx/64,playery/64):
+                if terrain_effect(playerx/64,playery/64)=="impassable": #undo if you hit a wall
                     self.rect.move_ip(64,0)
 
                     
@@ -76,13 +84,13 @@ class Player(pygame.sprite.Sprite):
                 xshift-=64
                 playerx=self.rect.left-xshift
                 playery=self.rect.top-yshift
-                if not is_passable(playerx/64,playery/64):
+                if terrain_effect(playerx/64,playery/64)=="impassable":
                     xshift+=64
             else:
                 self.rect.move_ip(64, 0)
                 playerx=self.rect.left-xshift
                 playery=self.rect.top-yshift
-                if not is_passable(playerx/64,playery/64):
+                if terrain_effect(playerx/64,playery/64)=="impassable":
                     self.rect.move_ip(-64,0)
 
                     
@@ -92,13 +100,13 @@ class Player(pygame.sprite.Sprite):
                 yshift+=64
                 playerx=self.rect.left-xshift
                 playery=self.rect.top-yshift
-                if not is_passable(playerx/64,playery/64):
+                if terrain_effect(playerx/64,playery/64)=="impassable":
                     yshift-=64
             else:
                 self.rect.move_ip(0, -64)
                 playerx=self.rect.left-xshift
                 playery=self.rect.top-yshift
-                if not is_passable(playerx/64,playery/64):
+                if terrain_effect(playerx/64,playery/64)=="impassable":
                     self.rect.move_ip(0,64)
 
                     
@@ -108,13 +116,13 @@ class Player(pygame.sprite.Sprite):
                 yshift-=64
                 playerx=self.rect.left-xshift
                 playery=self.rect.top-yshift
-                if not is_passable(playerx/64,playery/64):
+                if terrain_effect(playerx/64,playery/64)=="impassable":
                     yshift+=64
             else:
                 self.rect.move_ip(0, 64)
                 playerx=self.rect.left-xshift
                 playery=self.rect.top-yshift
-                if not is_passable(playerx/64,playery/64):
+                if terrain_effect(playerx/64,playery/64)=="impassable":
                     self.rect.move_ip(0,-64)
 
         #beekeys
@@ -126,19 +134,31 @@ class Player(pygame.sprite.Sprite):
         if key==107: #K
             for bee in bees:
                 bee.kill()
+                
+        if key==108: #L
+            self.damage(10)
 
+        playerx=self.rect.left-xshift
+        playery=self.rect.top-yshift
+        if terrain_effect(playerx/64,playery/64)=="health":
+            self.health+=5
+        return(self.health)
+        
 #Setup sprite groups
 all_sprites = pygame.sprite.Group()
 bees = pygame.sprite.Group()
+players = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
+players.add(player)
 yshift=0
 xshift=0
 
 #Main loop
 while True:
     pygame.time.delay(20)
-    
+
+    #key events    
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE: #Quit
@@ -148,8 +168,21 @@ while True:
                 quit()
                 exit()
             else:
-                player.update(event.key) #Send keys to player.update
-                                                
+                #Render map
+                for x in range (0,150):
+                    for y in range (0,150):
+                        screen.blit(themap[y][x]["sprite"], [x*64+xshift,y*64+yshift]) # this is backwards innefficient
+
+                #render entities    
+                for entity in all_sprites:
+                    screen.blit(entity.image, entity.rect)
+                                    
+                #update bees
+                for bee in bees:
+                    bee.update()
+                    
+                pygame.draw.rect(screen, honeycolor, pygame.Rect(0, 900-64, player.update(event.key)*10, 64))
+                                                                    
         elif event.type == QUIT: #Quit
             pygame.quit()
             sys.exit()
@@ -157,22 +190,13 @@ while True:
             quit()
             exit()
 
-    #update bees
-    for bee in bees:
-        bee.update()
         
     #collisions
 
+    if pygame.sprite.groupcollide(players, bees, False, True, collided=None):
+        player.damage(10)
 
-
-    #Display map
-    for x in range (0,150):
-        for y in range (0,150):
-            screen.blit(themap[y][x]["sprite"], [x*64+xshift,y*64+yshift])
-
-    #render entities    
-    for entity in all_sprites:
-        screen.blit(entity.image, entity.rect)
+    
         
     #flip
     pygame.display.flip()
